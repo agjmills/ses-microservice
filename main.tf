3025651e7917a9d89da3f3d07d89fafecc51a785
Alex Mills <alex@alexmills.uk>
2025-02-16 15:25:32 +0000

    added deployment stage

--- main.tf
+++ main.tf
@@ -190,6 +190,24 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   depends_on = [aws_api_gateway_method_response.options_200]
 }
 
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.this.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "this" {
+  deployment_id = aws_api_gateway_deployment.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  stage_name    = "prod"
+}
+
 resource "aws_dynamodb_table" "this" {
   name         = "DomainToEmailTable"
   billing_mode = "PAY_PER_REQUEST"
